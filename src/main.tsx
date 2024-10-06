b40361b195b58b87a79cefa5e4672a18cd06323c
Alain <alain.devcs@gmail.com>
2024-10-06 05:47:52 +0200

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// eslint-disable-next-line
 import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 );
