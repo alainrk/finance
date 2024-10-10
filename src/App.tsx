76216abbc6a72c43238aa200210e1cf8a3597c2f
Alain <alain.devcs@gmail.com>
2024-10-10 05:23:10 +0200

    Minor fixes on tabs

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
         Financial Calculators
       </h1>
 
-      <div className="w-full max-w-md px-2 sm:px-0">
+      <div className="w-full px-2 sm:px-4 md:px-6 lg:px-8 xl:px-16 max-w-7xl">
         <div className="sm:hidden mb-2">
           <label htmlFor="tabs" className="sr-only">
             Select a tab
@@ -30,7 +30,9 @@ const App: React.FC = () => {
             }
           >
             <option value="mortgage">Mortgage Scenarios Simulator</option>
-            <option value="comparison">Rent vs Buy vs Mortage Simulator</option>
+            <option value="comparison">
+              Rent vs Buy vs Mortgage Simulator
+            </option>
           </select>
         </div>
         <div className="hidden sm:block">
@@ -41,7 +43,7 @@ const App: React.FC = () => {
                 activeTab === "mortgage"
                   ? "bg-indigo-100 text-indigo-700"
                   : "text-gray-500 hover:text-gray-700"
-              } px-4 py-2 font-medium text-sm rounded-md transition-colors duration-150`}
+              } flex-1 px-4 py-2 font-medium text-sm sm:text-base md:text-lg rounded-md transition-colors duration-150 text-center`}
             >
               Mortgage Scenarios Simulator
             </button>
@@ -51,15 +53,15 @@ const App: React.FC = () => {
                 activeTab === "comparison"
                   ? "bg-indigo-100 text-indigo-700"
                   : "text-gray-500 hover:text-gray-700"
-              } px-4 py-2 font-medium text-sm rounded-md transition-colors duration-150`}
+              } flex-1 px-4 py-2 font-medium text-sm sm:text-base md:text-lg rounded-md transition-colors duration-150 text-center`}
             >
-              Rent vs Buy vs Mortage Simulator
+              Rent vs Buy vs Mortgage Simulator
             </button>
           </nav>
         </div>
       </div>
 
-      <div className="mt-4 w-full max-w-6xl px-4 sm:px-6 lg:px-8">
+      <div className="mt-4 w-full px-2 sm:px-4 md:px-6 lg:px-8 xl:px-16 max-w-7xl">
         {activeTab === "mortgage" ? (
           <MortgageCalculator />
         ) : (
